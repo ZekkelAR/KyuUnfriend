b889aab2c12d889b56a0d59ff100f300105c6980
Zekkel AR <40993331+ZekkelAR@users.noreply.github.com>
2022-03-07 16:29:36 +0700

    Update Generate_Token.py

--- Unf/Generate_Token.py
+++ Unf/Generate_Token.py
@@ -10,8 +10,8 @@ class Kyu:
 	def nani(self):
 		s = requests.Session()
 		urlgenerate = ("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email={}&locale=en_US&password={}&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6" .format(self.username, self.password))
-		meki = s.get(urlgenerate).text
-		zz=json.loads(meki)
+		reks1 = s.get(urlgenerate).text
+		zz=json.loads(reks1)
 		try:
 			yyy=(zz['access_token'])
 			print (yyy)
